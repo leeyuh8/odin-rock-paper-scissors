b1e31e22125178776b58c49d38cce0f0e7cecdf1
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-16 13:19:10 -0700

    Rename choiceComputer to computer Selection

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
 let options = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice() { 
-    let choiceComputer = options[Math.floor(Math.random() * 3)];
-    return choiceComputer;
+    let computerSelection = options[Math.floor(Math.random() * 3)];
+    return computerSelection;
 }
 
+console.log(getComputerChoice());
+
+
+function oneRound(playerSelection, computerSelection) {
+
+}
 
 
 
