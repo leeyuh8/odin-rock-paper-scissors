d20d1b081010e5a21309d895a2f8870c66b0a4d8
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-16 16:06:35 -0700

    Edit return strings of playRound and draft for loop of game

--- javascript.js
+++ javascript.js
@@ -21,29 +21,29 @@ function playRound(playerSelection, computerSelection) {
                 return 'Tie!';
                 break;
             case 'Paper':
-                return 'Computer wins! Paper beats Rock.';
+                return 'Computer wins!';
                 break;
             case 'Scissors':
-                return 'You win! Rock beats Scissors.';
+                return 'You win!';
         }
     } else if (playerSelection.toLowerCase() === 'paper') {
         switch (computerSelection) {
             case 'Rock':
-                return 'You win! Paper beats Rock.';
+                return 'You win!';
                 break;
             case 'Paper':
                 return 'Tie!';
                 break;
             case 'Scissors':
-                return 'Computer wins! Scissors beats Paper.';
+                return 'Computer wins!';
         }
     } else if (playerSelection.toLowerCase() === 'scissors') {
         switch (computerSelection) {
             case 'Rock':
-                return 'Computer wins! Rock beats Scissors.';
+                return 'Computer wins!';
                 break;
             case 'Paper':
-                return 'You win! Scissors beats Paper.';
+                return 'You win!';
                 break;
             case 'Scissors':
                 return 'Tie!';
@@ -55,4 +55,30 @@ function playRound(playerSelection, computerSelection) {
 console.log(playRound(playerSelection, computerSelection)); 
 
 
+let scorePlayer = 0;
+let scoreComputer = 0;
+function game(playRound) {
+    // run game five times
+    // in each iteration: 
+    //   - get computerSelection
+    //   - get player Selection
+    //   - get result of running playRound
+    //   - store result as a tally to scorePlayer or score Computer
+    for (var i = 0; i < 4; i++) {
+        let result = playRound(playerSelection, computerSelection);
+        if (result === 'You win!') {
+            return ++scorePlayer;
+        } else if (result === 'Computer wins!') {
+            return ++scoreComputer;
+        } else if (result === 'Tie!') {
+            return ++scorePlayer && ++scoreComputer;
+        }
+    }
 
+
+    //  compare player score to computer score:
+    //   - playerScore > computerScore, then player wins!
+    //   - computerScore > playerScore, then computer wins!
+    //   - playerScore = computerScore, then tie!
+}
+console.log(game(playRound));
