cfc21caf49b940f0e099517aa02f223e469b2e31
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-28 17:45:23 -0700

    Update displayWinner() to remove children of divParent

--- javascript.js
+++ javascript.js
@@ -2,9 +2,11 @@ let options = ['Rock', 'Paper', 'Scissors'];
 let computerSelection;
 let playerSelection;
 
+const divParent = document.querySelector('div.parent');
 const divResult = document.querySelector('div.result');
 const spanPlayerScore = document.querySelector('span.player-score');
 const spanComputerScore = document.querySelector('span.computer-score')
+
 
 
 
@@ -55,32 +57,49 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 
-/*  - display score if each player in corresponding span
-        - every time a round plays, need to increment the winners score
-    - once a player reached 5 points, display winning player
-
-    read the result in div.result.
+/*  read the result in div.result.
         - if it says 'Computer wins', increment the score in span.computer-score
         - if it says 'You win', increment the score in span.player-score
-
 */
-
     if (divResult.textContent === 'You win!') {
         ++spanPlayerScore.textContent;
     } else if (divResult.textContent === 'Computer wins!') {
         ++spanComputerScore.textContent;
     } 
 
+/* check to see if player or computer score is 5
+        -if player score is 5, run displayWinner() so that divWinner
+        states 'You are the winner!'
+        -if computer score is 5, run displayWinner() so that divWinner
+        states states the 'Computer wins!'
+
+        -must first check for a tie, if so, return 'Tie!'
+*/
+    if (spanPlayerScore.textContent === '5' && spanComputerScore.textContent === '5') {
+        console.log('works');
+    } else if (spanPlayerScore.textContent === '5' ) {
+        console.log('works');
+    } else if (spanComputerScore.textContent === '5') {
+        console.log('works');
+    }
+
+
 }
 
 
-
-
+// function to remove span.player-score, span.computer-score
+// and div.result, add replaces it with h1.winner 
+function displayWinner() {
+    divParent.textContent = '';
+    const h1Winner = document.createElement('h1');
+    divParent.appendChild(h1Winner);
+}
 
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', playRound));
 
+displayWinner();
 
 
 
